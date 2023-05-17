a7930c74c3547c88b47830d99c5fba1b1071cc33
chrisw889 <chrisjworth1@gmail.com>
2023-05-17 09:39:52 +0200

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+const App = () => (
+  <>
+    <div className="app-header">
+      Treeconomy Challenge
+      <span className="app-header-name">Christopher Worthington</span>
+    </div>
+  </>
+);
+
+export default App;
